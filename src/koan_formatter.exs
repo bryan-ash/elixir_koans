e65d96e7ae8ee503e85eb4f6bf8206e9dbfd96d5
Bryan Ash <bryan.a.ash@gmail.com>
2013-10-02 00:11:41 -0400

    A passing test expands awareness, a failing damages karma and halts.

--- src/koan_formatter.exs
+++ src/koan_formatter.exs
@@ -21,13 +21,17 @@ defmodule KoanFormatter do
     :ok
   end
 
-  def test_finished(:ok, test) do
-    ExUnit.Test[name: description, case: test_case, failure: failure] = test
+  def test_finished(:ok, ExUnit.Test[name: description, case: test_case, failure: ({:error, _, _} = failure)]) do
     {_kind, _reason, [{_, _, _, [file: file, line: line]}]} = failure
     # test_name = String.split(name, %r/^test /)
 
     IO.puts formatted_test_failure(test_case, description, Path.relative_to_cwd(file), line)
     System.halt(0)
+  end
+
+  def test_finished(:ok, ExUnit.Test[name: description, case: test_case]) do
+    IO.puts formatted_test_success(test_case, description)
+    :ok
   end
 
   def formatted_test_failure(test_case, description, file, line) do
@@ -36,7 +40,15 @@ defmodule KoanFormatter do
 			red("  ./#{file}:#{line}, in '#{description}'")
 	end
 
+  def formatted_test_success(test_case, description) do
+		green("#{inspect(test_case)} '#{description}' has expanded your awareness.")
+	end
+
 	defp red(string) do
 		IO.ANSI.escape("%{red}" <> string)
+	end
+
+	defp green(string) do
+		IO.ANSI.escape("%{green}" <> string)
 	end
 end
