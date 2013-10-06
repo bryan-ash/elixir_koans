775483d16859f2492bc82cd3f07c0a6ba2331965
Bryan Ash <bryan.a.ash@gmail.com>
2013-10-05 23:51:47 -0400

    remove whitespace

--- mix.exs
+++ mix.exs
@@ -35,18 +35,18 @@ end
 
 defmodule Mix.Tasks.Walk_the_path do
   use Mix.Task
- 
+
   @shortdoc "Procede along the path to Elixir enlightenment"
- 
+
   def run(_) do
     Mix.Task.run "gen"
     Code.require_file "path_to_enlightenment.exs", "lib"
   end
 end
- 
+
 defmodule ElixirKoans.Mixfile do
   use Mix.Project
- 
+
   def project do
     [ app: :elixir_koans,
       version: "0.0.1",
@@ -54,12 +54,12 @@ defmodule ElixirKoans.Mixfile do
       deps: deps,
       default_task: :walk_the_path ]
   end
- 
+
   # Configuration for the OTP application
   def application do
     []
   end
- 
+
   # Returns the list of dependencies in the format:
   # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
