19cbd793aa79eb97d06af2ce1f7706c0b3784485
Bryan Ash <bryan.a.ash@gmail.com>
2013-10-12 13:32:29 -0400

    Use GenServer to hold state

--- lib/koan_formatter.exs
+++ lib/koan_formatter.exs
@@ -1,25 +1,81 @@
 defmodule KoanFormatter do
   @behaviour ExUnit.Formatter
 
-  def suite_started(_opts), do: :ok
-  def suite_finished(:ok, _run_us, _load_us), do: 1
-  def case_started(:ok, _test_case), do: :ok
-  def case_finished(:ok, _test_case), do: :ok
-  def test_started(:ok, _test), do: :ok
+  use GenServer.Behaviour
 
-  def test_finished(:ok,
+  defrecord State, successes: 0, failures: 0
+
+  ## Behaviour
+
+  def suite_started(opts) do
+    { :ok, pid } = :gen_server.start_link(__MODULE__, opts, [])
+    pid
+  end
+
+  def suite_finished(id, run_us, load_us) do
+    :gen_server.call(id, { :suite_finished, run_us, load_us }, @timeout)
+  end
+
+  def case_started(id, test_case) do
+    :gen_server.cast(id, { :case_started, test_case })
+  end
+
+  def case_finished(id, test_case) do
+    :gen_server.cast(id, { :case_finished, test_case })
+  end
+
+  def test_started(id, test) do
+    :gen_server.cast(id, { :test_started, test })
+  end
+
+  def test_finished(id, test) do
+    :gen_server.cast(id, { :test_finished, test })
+  end
+
+  ## Callbacks
+
+  def init(opts) do
+    { :ok, State.new(opts) }
+  end
+
+  def handle_call({ :suite_finished, _run_us, _load_us }, _from, state = State[]) do
+    { :stop, :normal, state.failures, state }
+  end
+
+  def handle_call(reqest, from, state) do
+    super(reqest, from, state)
+  end
+
+  def handle_cast({ :test_started, ExUnit.Test[] = _test }, state) do
+    { :noreply, state }
+  end
+
+  def handle_cast({ :test_finished,
                     ExUnit.Test[failure: ({:error,
                                            ExUnit.ExpectationError[] = record,
-                                           [{test_case, test_name, _, [file: file, line: line]}]})]) do
+                                                  [{test_case, test_name, _, [file: file, line: line]}]})] },
+                  _state = State[]) do
     IO.puts formatted_test_failure(test_case, test_name,
                                    record.prelude, record.expected, record.actual, record.assertion,
                                    Path.relative_to_cwd(file), line)
     System.halt(0)
   end
 
-  def test_finished(:ok, ExUnit.Test[name: test_name, case: test_case]) do
+  def handle_cast({ :test_finished, ExUnit.Test[name: test_name, case: test_case] }, _state = State[]) do
     IO.puts formatted_test_success(test_case, test_name)
     :ok
+  end
+
+  def handle_cast({ :case_started, ExUnit.TestCase[name: _name] }, state) do
+    { :noreply, state }
+  end
+
+  def handle_cast({ :case_finished, _test_case }, state) do
+    { :noreply, state }
+  end
+
+  def handle_cast(request, state) do
+    super(request, state)
   end
 
   def formatted_test_failure(test_case, test_name, prelude, expected, actual, assertion, file, line) do
