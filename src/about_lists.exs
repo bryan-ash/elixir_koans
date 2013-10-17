591f91b712c3c5e5433fa1745fd5ec65b51a5f29
Bryan Ash <bryan.a.ash@gmail.com>
2013-10-16 21:49:46 -0400

    add koan for List head, tail

--- src/about_lists.exs
+++ src/about_lists.exs
@@ -4,7 +4,14 @@ defmodule AboutLists do
   use ElixirKoans.Helpers
 
   test "matching is used to extract values from a list" do
-    [1, x, 3, 4] = [1, 2, 3, 4]
+    [1, x, 3, y] = [1, 2, 3, 4]
     assert x == __
+    assert y == __
+  end
+
+  test "the first element is the head, the rest is the tail" do
+    [ head | tail ] = [1, 2, 3, 4]
+    assert head == __
+    assert tail == __
   end
 end
